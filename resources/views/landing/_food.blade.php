d3373cc1a3be5d594edbd44d50abe6db19510e1d
Ryan Hayden <pastorryanhayden@gmail.com>
2023-04-19 07:02:07 -0500

    changed some working

--- resources/views/landing/_food.blade.php
+++ resources/views/landing/_food.blade.php
@@ -2,9 +2,8 @@
 <h2 class="text-center text-3xl font-display my-6">Each meal comes with...</h2>
     <div class="w-full max-w-5xl mx-auto grid lg:grid-cols-4 gap-6">
         <x-food-item name="Smoked Pulled Pork" image="/pork.jpg" description="Three pounds (about enough to feed six adults) of pulled pork cooked low and slow on a smoker for at least 12 hours." />
-        <x-food-item name="BBQ Beans" image="/beans.jpg" description="Our beans our cooked low and slow and include just the right mix of beans and spices.  They go perfectly with our pulled pork." />
-        <x-food-item name="Smoked Mac and Cheese" image="/macandcheese.jpg" description="The cheesiest, smokiest homemade mac and cheese you will ever have." />
+        <x-food-item name="Rhino Beans" image="/beans.jpg" description="Our scratch made beans our cooked low and slow with bacon and just the right mix of herbs and spices.  They go perfectly with our pulled pork." />
+        <x-food-item name="Smoked Mac and Cheese" image="/macandcheese.jpg" description="Four types of cheese. Smoke. This will be the cheesiest, smokiest homemade mac and cheese you will ever have." />
         <x-food-item name="Sweet Corn Bread" image="/cornbread.jpg" description="Sweet, moist, the perfect compliment to the rest of the meal." />
-        
         </div>
 </section>
--- resources/views/landing/_howitworks.blade.php
+++ resources/views/landing/_howitworks.blade.php
@@ -1,7 +1,7 @@
 <section class="howitworks w-full py-12 px-6 max-w-5xl mx-auto" id="howitworks">
         <h2 class="text-midnight text-2xl font-display text-center my-10">How It Works</h2>
         <div class="flex grid lg:grid-cols-5 gap-6">
-           <x-how-item title="We Schedule A Cook" description="We decide when we are going to cook next and prepare for that delivery.">
+           <x-how-item title="We Schedule A Cook" description="We decide when we are going to cook next (usually every Monday) and prepare for that delivery.">
             <x-slot:svg>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-10 h-10 mb-4">
                     <path fill-rule="evenodd" d="M6.75 2.25A.75.75 0 017.5 3v1.5h9V3A.75.75 0 0118 3v1.5h.75a3 3 0 013 3v11.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V7.5a3 3 0 013-3H6V3a.75.75 0 01.75-.75zm13.5 9a1.5 1.5 0 00-1.5-1.5H5.25a1.5 1.5 0 00-1.5 1.5v7.5a1.5 1.5 0 001.5 1.5h13.5a1.5 1.5 0 001.5-1.5v-7.5z" clip-rule="evenodd" />
@@ -22,7 +22,7 @@
 </svg>
             </x-slot:svg>
            </x-how-item>
-           <x-how-item title="You pick it up in Mattoon" description="On the delivery day, you pick it up at our location (Bible Baptist Church) in Mattoon, IL.">
+           <x-how-item title="You pick it up in Mattoon" description="On the delivery day, you pick it up at our location (Bible Baptist Church) in Mattoon, IL after 3pm.">
             <x-slot:svg>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-10 h-10 mb-4">
                 <path fill-rule="evenodd" d="M11.54 22.351l.07.04.028.016a.76.76 0 00.723 0l.028-.015.071-.041a16.975 16.975 0 001.144-.742 19.58 19.58 0 002.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 00-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 002.682 2.282 16.975 16.975 0 001.145.742zM12 13.5a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd" />
